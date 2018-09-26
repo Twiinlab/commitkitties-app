a1c76a12ae781ceca4cdf4a75a1906c62f4c8bff
david chavarri <dvdchavarri@davids-MacBook-Pro.local>
2018-09-26 22:05:18 +0200

    init firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,40 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+// firebase init goes here
+const config = {
+    apiKey: "AIzaSyAQxPhK8Hag2-C0HUWS0HSkfSmmsrDMoW8",
+    authDomain: "commitkitties.firebaseapp.com",
+    databaseURL: "https://commitkitties.firebaseio.com",
+    projectId: "commitkitties",
+    storageBucket: "commitkitties.appspot.com",
+    messagingSenderId: "1020750963599"
+}
+firebase.initializeApp(config)
+
+// firebase utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+const currentUser = auth.currentUser
+
+// date issue fix according to firebase
+const settings = {
+    timestampsInSnapshots: true
+}
+db.settings(settings)
+
+// firebase collections
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+export {
+    db,
+    auth,
+    currentUser,
+    usersCollection,
+    postsCollection,
+    commentsCollection,
+    likesCollection
+}
