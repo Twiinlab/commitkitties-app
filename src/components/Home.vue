0d2ea4100d9fbabf7119a590206f9881348bfba4
dvdchavarri@gmail.com <dvdchavarri@gmail.com>
2018-11-05 01:08:24 +0100

    profile update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
                 <div class="Explainer-what-text">
                     <h2 class="Explainer-headline">Lets kill the hype, BlockChain gets real</h2>
                     <p class="Explainer-description">En la charla desmitificaremos blockchain y las criptomonedas, analizando qué hay realmente detrás de este espectáculo de luces y sonido. Desmontando una de las plataformas más populares en la actualidad sobre Ethereum y analizaremos las claves de su funcionamiento, sus costes y métricas.</p>
-                    <div><md-button href="https://www.koliseo.com/events/commit-2018/r4p/5630471824211968/agenda#/5734118109216768/5643309078806528" class="md-raised">Go to the Talk</md-button></div>
+                    <div><md-button href="https://www.koliseo.com/events/commit-2018/r4p/5630471824211968/agenda#/5734118109216768/5643309078806528" class="md-raised md-primary">Go to the Talk</md-button></div>
                 </div>
             </div>
         </section>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,28 +1,84 @@
 <template>
-    <section id="settings" class="explainer-bg">
-        <div class="col1">
-            <h3>Profile</h3>
-            <p>Update your info</p>
-
-            <transition name="fade">
-                <p v-if="showSuccess" class="success">profile updated</p>
-            </transition>
-
-            <form @submit.prevent>
-                <label for="name">Display Name</label>
-                <input v-model.trim="displayName" type="text" :placeholder="userProfile.displayName" id="displayName" />
-
-                <label for="title">Email</label>
-                <input v-model.trim="email" type="text" :placeholder="userProfile.email" id="email" />
-
-                <button @click="updateProfile" class="button">Update Profile</button>
-            </form>
-        </div>
-    </section>
+    <div id="dashboard" class="explainer-bg">
+        <section>
+            <div class="col1">
+                <div class="profile">
+                    <h3>Profile</h3>
+                    <p>Update your info</p>
+                    <transition name="fade">
+                        <p v-if="showSuccess" class="success">profile updated</p>
+                    </transition>
+                    <form @submit.prevent>
+                        <label for="name">Display Name</label>
+                        <input v-model.trim="displayName" type="text" :placeholder="userProfile.displayName" id="displayName" />
+                        <label for="title">Email</label>
+                        <input v-model.trim="email" type="text" :placeholder="userProfile.email" id="email" />
+                        <button @click="updateProfile" class="button">Update Profile</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col2">
+                <div>
+                    <md-tabs md-sync-route>
+                        <md-tab id="tab-home" md-label="Kitties" >
+                            <md-card v-if="myKitties.length == 0">
+                                <md-card-media>
+                                    <img src="@/assets/images/noKitty.png" alt="logo">
+                                </md-card-media>
+                                <md-card-header>
+                                    <div class="md-subhead">You don´t own kitties yet</div>
+                                </md-card-header>                        
+                            </md-card>
+                            <div class="scrolling-wrapper">
+                                <router-link :to="{ name: 'Kitty', params: { id: kitty.id}}" v-if="kitty.image_url" v-for="kitty in myKitties" v-bind:key="kitty.id">
+                                    <md-card >
+                                        <md-card-media>
+                                            <img v-if="kitty.image_url" v-bind:src="kitty.image_url" alt="People">
+                                        </md-card-media>
+                                        <md-card-header>
+                                            <div class="md-subhead">{{kitty.name|truncate(30)}}</div>
+                                            <div class="md-subhead">Ξ  0.020</div>
+                                        </md-card-header>                           
+                                    </md-card>
+                                </router-link>
+                            </div>
+                        </md-tab>
+                        <md-tab id="tab-pages" md-label="Activity" >
+                            <md-list class="md-double-line">
+                                <md-list-item>
+                                    <md-icon class="md-primary">shopping_cart</md-icon>
+                                    <div class="md-list-item-text">
+                                        <span>Kitty Purchase</span>
+                                        <span>Ξ  0.050</span>
+                                    </div>
+                                </md-list-item>
+                                <md-divider class="md-inset"></md-divider>
+                                <md-list-item>
+                                    <md-icon class="md-primary">swap_horiz</md-icon>
+                                    <div class="md-list-item-text">
+                                        <span>Kitty Exchange</span>
+                                        <span>Ξ  0.065</span>
+                                    </div>
+                                </md-list-item>
+                                <md-divider class="md-inset"></md-divider>
+                                <md-list-item>
+                                    <md-icon class="md-primary">add_shopping_cart</md-icon>
+                                    <div class="md-list-item-text">
+                                        <span>Kitty Publication</span>
+                                        <span>Ξ  0.135</span>
+                                    </div>
+                                </md-list-item>
+                            </md-list>
+                        </md-tab>
+                    </md-tabs>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
+    import { mapState, mapGetters, mapActions  } from 'vuex'
 
     export default {
         data() {
@@ -32,10 +88,15 @@
                 showSuccess: false
             }
         },
+        created() {
+            this.fetchMyKitties('Ix0Mo3CbhbegnTft36X0yCUWnhJ3');
+        },
         computed: {
-            ...mapGetters('users', ['userProfile'])
+            ...mapGetters('users', ['userProfile']),
+            ...mapGetters('kitties', ['myKitties'])
         },
         methods: {
+            ...mapActions('kitties', ['fetchMyKitties']),
             updateProfile() {
                 this.$store.dispatch('users/updateProfile', {
                     displayName: this.displayName !== '' ? this.displayName : this.userProfile.displayName,
@@ -52,3 +113,22 @@
         }
     }
 </script>
+
+<style lang="scss" scoped>
+  .profile{
+      display: grid;
+  }
+
+  .scrolling-wrapper{
+    //overflow-x: scroll;
+    overflow-x: auto;
+    overflow-y: hidden;
+    white-space: nowrap;
+  }
+  .md-card {
+    width: 240px;
+    margin: 4px;
+    display: inline-block;
+    vertical-align: top;
+  }
+</style>
