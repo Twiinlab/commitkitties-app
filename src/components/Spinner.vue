01052cfe960830f158bbccad2137bbc7a4421d5a
dvdchavarri@gmail.com <dvdchavarri@gmail.com>
2018-11-09 17:47:02 +0100

    Img reference update

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -14,13 +14,9 @@
 </template>
 
 <script>
-import { HalfCircleSpinner } from "epic-spinners";
 
 export default {
   name: "Spinner",
-  components: {
-    HalfCircleSpinner
-  },
   props: {
     condition: {
       type: Boolean,
