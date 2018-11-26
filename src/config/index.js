4e437cf89dce72b44ce4f81c4e2e44c23fe5e253
ruben chavarri <ruben.chavarri.contractor@bbva.com>
2018-11-26 01:17:39 +0100

    update dist file

--- src/config/index.js
+++ src/config/index.js
@@ -20,8 +20,8 @@ const custom = {
       base: '/api'
     },
     network:{
-      http: "http://localhost:8545",
-      ws: "ws://localhost:8545",
+      http: "http://35.243.198.50:8545", //http: "http://localhost:8545",
+      ws: "ws://35.243.198.50:8545", //ws: "ws://localhost:8545",
       account: {
         address: "0x4aaa4e3ce8e9d8a6533b75db54da017e2cf811c8",
         key: "0xbb73e8a2733d614b215c1a651dd3884cabf1149a6dc847f1b6ce20c9d2f682ce"
@@ -29,7 +29,7 @@ const custom = {
       mnemonic: "cargo little forum bring connect ready old matter accident ability never thumb"
     } 
   },
-  pre: {
+  production: {
     firebase: {
       apiKey: "AIzaSyAneLpLkRE7lLi2tFasaQHXs18FuLBz1IY",
       authDomain: "commitkitties-geth.firebaseapp.com",
@@ -43,8 +43,8 @@ const custom = {
       base: '/api'
     },
     network:{
-      http: "http://35.196.98.185:8545",
-      ws: "ws://35.196.98.185:8545",
+      http: "http://35.243.198.50:8545",
+      ws: "ws://35.243.198.50:8545",
       account: {
         address: "0x4aaa4e3ce8e9d8a6533b75db54da017e2cf811c8",
         key: "0xbb73e8a2733d614b215c1a651dd3884cabf1149a6dc847f1b6ce20c9d2f682ce"
@@ -66,8 +66,8 @@ const custom = {
       base: '/api'
     },
     network: {
-      http:  "https://rinkeby.infura.io/v3/b97d8303c3d84cf8bb43c325612ae8a7", //"https://rinkeby.infura.io/2nluVzjZVelxFadFKD0f",
-      ws: "wss://rinkeby.infura.io/ws", //"wss://rinkeby.infura.io/v3/b97d8303c3d84cf8bb43c325612ae8a7", //"wss://rinkeby.infura.io/2nluVzjZVelxFadFKD0f",
+      http:  "https://mainnet.infura.io/v3/365492c4737f413c9bccb6a52e72c356", //"https://rinkeby.infura.io/v3/b97d8303c3d84cf8bb43c325612ae8a7", //"https://rinkeby.infura.io/2nluVzjZVelxFadFKD0f",
+      ws: "wss://rinkeby.infura.io/_ws", //"wss://rinkeby.infura.io/v3/b97d8303c3d84cf8bb43c325612ae8a7", //"wss://rinkeby.infura.io/2nluVzjZVelxFadFKD0f",
       account: {
           address: "0x8420AaB0b1a42c26089e696FBeF3e35ec9Da2849", //"0x359247c938ea24Aef9e30fDD8eaa0F39DCbB2249",
           key: "0x79A2227E181F91A2CFE1C89394A2BF0D530B83952A83928770F3ABB2070A5F30" //"0x92EAE2BCD7BCF17BF9FA0E82DE08393BF5D67AB9203C704A95CC793F25A1E37C"
